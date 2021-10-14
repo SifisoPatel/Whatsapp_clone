0e18a7fe5a45d858265235484d0689008f55849a
Sifiso Patel <sfsptl@protonmail.com>
2021-10-14 09:02:21 +0200

    initialised the project, added sidebar pages and initialised on firebase

---
+++ README.txt
@@ -0,0 +1,7 @@
+WhatsApp clone from a youtube tutorial using the MERN stack
+
+*initiated firebase
+*installed es7 extension on VS Code
+*deleted unused/obsolete pages
+*brought in firebase config file into firebase.js
+*created sidebar.js file
---
+++ whatsapp-mern/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a MERN whatsapp clone</h1>
+
+      {/*Sidebar*/}
+
+      {/*Chat component*/}
+
+    </div>
+  );
+}
+
+export default App;
