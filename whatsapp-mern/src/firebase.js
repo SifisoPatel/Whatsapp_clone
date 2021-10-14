df9db369165ced1933bc3402e316450a91e19bfa
Sifiso Patel <sfsptl@protonmail.com>
2021-10-14 09:04:20 +0200

    forgot to include the firebase.js file

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCX-WCOorM63c89X8HPcfMbEoNXV3nD9bg",
+    authDomain: "whatsapp-mern-ced86.firebaseapp.com",
+    projectId: "whatsapp-mern-ced86",
+    storageBucket: "whatsapp-mern-ced86.appspot.com",
+    messagingSenderId: "195082352112",
+    appId: "1:195082352112:web:747ba4a4c8dbc38b4f5935"
+};
