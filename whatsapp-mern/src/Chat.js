cb74983be5cc273f2b0946b18b448232d6507334
Sifiso Patel <sfsptl@protonmail.com>
2021-11-15 20:32:00 +0200

    Finished off sidebar comp ... Started on the message comp

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from 'react';
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am a chat window</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Sifiso</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+
+                </p>
+            </div>
+
         </div>
     )
 }
